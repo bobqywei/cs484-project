06cf0d55093d3351afbb401d33f9d71bb7c5d19d
Bob Wei <bobqywei@gmail.com>
2020-12-17 01:14:13 -0500

    Added training loss computations

---
+++ loss.py
@@ -0,0 +1,121 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from utils import *
+
+
+class BaseLoss(object):
+    def __init__(self, config):
+        self.config = config
+        self.H = config['img_hgt']
+        self.W = config['img_wid']
+        self.bsize = config['batch_size']
+
+        x, y = np.meshgrid(range(self.W), range(self.H))
+        xy_coords = np.stack([x, y], axis=0).astype(np.float32)
+        xy_coords = torch.from_numpy(xy_coords).view(1, 2,-1).to(config['device']) # [1,2,H*W]
+        xy_coords = xy_coords.repeat(self.bsize, 1, 1) # [B,2,H*W]
+
+        self.ones = torch.ones(self.bsize, 1, self.H * self.W, device=config['device'])
+        # 2D image plane coords to homogeneous 3D coords
+        self.img_coords = torch.cat([xy_coords, self.ones], dim=1) # [B,3,H*W]
+
+        self.SSIMLoss = SSIM()
+
+    def elemwise_reprojection_loss(self, pred, target):
+        l1_loss = torch.abs(pred - target).mean(dim=1)
+        ssim_loss = self.SSIMLoss(pred, target).mean(dim=1)
+        return ssim_loss*0.85 + l1_loss*0.15
+
+    # t_img, tPrime_img: [B,3,H,W]
+    # Ks: 4 x [B,4,4]
+    # invKs: 4 x [B,4,4]
+    # pose_preds (for t -> t'): [B,6]
+    # disparity_preds (for t): 4 x [B,scaleH,scaleW]
+    def compute_proj_loss(self, t_img, tPrime_img, Ks, invKs, pose_preds, disparity_preds, invert=False):
+        bsize = t_img.shape[0]
+
+        R = rotation_matrix(pose_preds[:,:3])
+        T = translation_matrix(pose_preds[:,3:])
+        P = torch.matmul(R.transpose(1,2), T*-1) if invert else torch.matmul(T, R) # [B,4,4]
+
+        depth_preds = disparity_to_depth(disparity_preds, self.config['min_depth'], self.config['max_depth'])
+
+        total_proj_loss = 0
+
+        for i in range(self.config['num_scales']):
+            depth = depth_preds[i]
+            # Upsample all depth maps to original [B,H,W]
+            if i > 0:
+                depth = F.interpolate(
+                    depth, (self.H, self.W), mode="bilinear", align_corners=False)
+
+            K = Ks[i]
+            invK = invKs[i]
+
+            # project from camera t normalized image coords to coords w.r.t. camera t'
+            cam_t_coords = torch.matmul(invK[:,:3,:3], self.img_coords) # [B,3,H*W]
+            # gives us (d*x, d*y, d, 1) homogeneous coords
+            cam_t_coords = depth.view(bsize, 1, -1) * cam_t_coords
+            cam_t_coords = torch.cat([cam_t_coords, self.ones], dim=1) # [B,4,H*W]
+            
+            # projection matrix to camera t' normalized image coords 
+            # don't care about 4th coordinate in homogeneous representation
+            proj_tPrime = torch.matmul(K, P)[:,:3,:]
+            tPrime_coords = torch.matmul(proj_tPrime, cam_t_coords) # [B,3,H*W]
+
+            # homogeneous coords to xy coords
+            tPrime_coords = tPrime_coords[:,:2,:] / (tPrime_coords[:,2:3,:] + 1e-10)
+            tPrime_coords = tPrime_coords.view(bsize, 2, self.H, self.W).permute(0, 2, 3, 1) # [B,H,W,2]
+            # normalize t' image coords to [-1, 1]
+            tPrime_coords[:,:,:,0] /= self.W - 1
+            tPrime_coords[:,:,:,1] /= self.H - 1
+            tPrime_coords = (tPrime_coords - 0.5) * 2
+            
+            # form the predicted rgb image for t'
+            pred_tPrime_img = F.grid_sample(t_img, tPrime_coords, padding_mode="border")
+
+            # compute the reprojection loss
+            reproj_loss = self.elemwise_reprojection_loss(pred_tPrime_img, tPrime_img)
+            # use automasking for removing stationary pixels from loss
+            if self.config['use_mask']:
+                unwarped_loss = self.elemwise_reprojection_loss(t_img, tPrime_img)
+                keep_mask = (reproj_loss < unwarped_loss)
+                reproj_loss = reproj_loss[keep_mask]
+            
+            total_proj_loss += reproj_loss.mean()
+
+        return total_proj_loss / self.config['num_scales']
+
+
+    # disp_imgs (for t): 4 x [B,scaleH,scaleW]
+    # rgb_img (for t): [B,3,H,W] 
+    def compute_smooth_loss(self, disp_imgs, rgb_img):
+        # using rgb image for simple object edge awareness
+        grad_rgb_x = torch.mean(torch.abs(rgb_img[:, :, :, :-1] - rgb_img[:, :, :, 1:]), dim=1, keepdim=True)
+        grad_rgb_y = torch.mean(torch.abs(rgb_img[:, :, :-1, :] - rgb_img[:, :, 1:, :]), dim=1, keepdim=True)
+        scale_x = torch.exp(-grad_rgb_x)
+        scale_y = torch.exp(-grad_rgb_y)
+
+        total_smooth_loss = 0
+        for scale in range(self.config['num_scales']):
+            disp_img = disp_imgs[scale]
+            if scale > 0:
+                disp_img = F.interpolate(
+                    disp_img, (self.H, self.W), mode="bilinear", align_corners=False)
+                
+            # normalize by the mean disparity
+            norm_disp = disp_img / (disp_img.mean(dim=2, keepdim=True).mean(dim=3, keepdim=True) + 1e-10)
+
+            grad_disp_x = torch.abs(norm_disp[:, :, :, :-1] - norm_disp[:, :, :, 1:])
+            grad_disp_y = torch.abs(norm_disp[:, :, :-1, :] - norm_disp[:, :, 1:, :])
+            grad_disp_x *= scale_x
+            grad_disp_y *= scale_y
+
+            smooth_loss = grad_disp_x.mean() + grad_disp_y.mean()
+            # total_smooth_loss += smooth_loss / (2 ** scale)
+            total_smooth_loss += smooth_loss
+
+        return self.config['smooth_weight'] * total_smooth_loss / self.config['num_scales']
--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import torch
-
+import torch.nn as nn
 
 def to_device(obj, device):
     if isinstance(obj, dict):
@@ -11,3 +11,83 @@ def to_device(obj, device):
     elif isinstance(obj, torch.Tensor):
         obj = obj.to(device)
     return obj
+
+
+def disparity_to_depth(disp, min_depth, max_depth):
+    max_disp = 1 / min_depth
+    min_disp = 1 / max_depth
+    depth = 1 / (min_disp + (max_disp - min_disp) * disp)
+    return depth
+
+
+def rotation_matrix(axisangles):
+    bsize = axisangles.shape[0]
+    R = torch.zeros((bsize, 4, 4), device=axisangles.device)
+    theta = torch.norm(axisangles, p=2, dim=1, keepdim=True)
+    axis = axisangles / (theta + 1e-10)
+    
+    a = axis[:, 0:1]
+    b = axis[:, 1:2]
+    c = axis[:, 2:3]
+
+    cos = torch.cos(theta)
+    icos = 1 - cos
+    sin = torch.sin(theta)
+
+    R[:,0,0] = cos + a**2*icos
+    R[:,0,1] = a*b*icos - c*sin
+    R[:,0,2] = a*c*icos + b*sin
+    R[:,1,0] = a*b*icos + c*sin
+    R[:,1,1] = cos + b**2*icos
+    R[:,1,2] = b*c*icos - a*sin
+    R[:,2,0] = a*c*icos - b*sin
+    R[:,2,1] = b*c*icos + a*sin
+    R[:,2,2] = cos + c**2*icos
+    R[:,3,3] = 1.0
+    return R
+
+
+def translation_matrix(translation):
+    bsize = translation.shape[0]
+    T = torch.zeros((bsize, 4, 4), device=translation.shape)
+
+    T[:,0,0] = 1.0
+    T[:,1,1] = 1.0
+    T[:,2,2] = 1.0
+    T[:,3,3] = 1.0
+    T[:,:3,3] = translation
+    return T
+
+
+# Source: https://github.com/nianticlabs/monodepth2/blob/ab2a1bf7d45ae53b1ae5a858f4451199f9c213b3/layers.py#L218
+class SSIM(nn.Module):
+    """Layer to compute the SSIM loss between a pair of images
+    """
+    def __init__(self):
+        super(SSIM, self).__init__()
+        self.mu_x_pool   = nn.AvgPool2d(3, 1)
+        self.mu_y_pool   = nn.AvgPool2d(3, 1)
+        self.sig_x_pool  = nn.AvgPool2d(3, 1)
+        self.sig_y_pool  = nn.AvgPool2d(3, 1)
+        self.sig_xy_pool = nn.AvgPool2d(3, 1)
+
+        self.refl = nn.ReflectionPad2d(1)
+
+        self.C1 = 0.01 ** 2
+        self.C2 = 0.03 ** 2
+
+    def forward(self, x, y):
+        x = self.refl(x)
+        y = self.refl(y)
+
+        mu_x = self.mu_x_pool(x)
+        mu_y = self.mu_y_pool(y)
+
+        sigma_x  = self.sig_x_pool(x ** 2) - mu_x ** 2
+        sigma_y  = self.sig_y_pool(y ** 2) - mu_y ** 2
+        sigma_xy = self.sig_xy_pool(x * y) - mu_x * mu_y
+
+        SSIM_n = (2 * mu_x * mu_y + self.C1) * (2 * sigma_xy + self.C2)
+        SSIM_d = (mu_x ** 2 + mu_y ** 2 + self.C1) * (sigma_x + sigma_y + self.C2)
+
+        return torch.clamp((1 - SSIM_n / SSIM_d) / 2, 0, 1)
